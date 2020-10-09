b4a4c75cd507c88c5c15effc44f3e8f5c1d612d1
iPocks54 <arthurel54@aol.com>
2020-10-09 16:37:28 +0900

    Parallax + Camera control

---
+++ LELOUP/Assets/Script/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float Speed = 5;
+
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * Speed / 100;
+
+        transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + 0);
+    }
+}
