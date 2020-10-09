a0c893196a2883a3d37a159b7f31dbba8ea3ae73
iPocks54 <arthurel54@aol.com>
2020-10-10 00:04:33 +0900

    Stash (enemy spawner)

---
+++ LELOUP/Assets/Script/EnemieSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieSpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    float randX;
+    Vector2 posSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-8.4f, 8.4f);
+            posSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(enemy, posSpawn, Quaternion.identity);
+        }
+    }
+}
