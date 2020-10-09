9c91821f71fab10247271c829579a8d1f9b116bc
iPocks54 <arthurel54@aol.com>
2020-10-10 02:09:24 +0900

    first enemy generation

--- LELOUP/Assets/Script/EnemieSpawner.cs
+++ LELOUP/Assets/Script/EnemieSpawner.cs
@@ -6,21 +6,31 @@ public class EnemieSpawner : MonoBehaviour
 {
 
     public GameObject enemy;
+    public GameObject player;
+    public bool right;
     float randX;
     Vector2 posSpawn;
     public float spawnRate = 2f;
     float nextSpawn = 0.0f;
+    Vector2 pos;
+    float offset = 15;
     void Start()
     {
-        
+        pos = player.transform.position;
+
+        if (!right)
+            offset = -15;
     }
 
     void Update()
     {
+        pos = player.transform.position;
+        transform.position = new Vector3(pos.x, 0, -10);
+
         if (Time.time > nextSpawn)
         {
             nextSpawn = Time.time + spawnRate;
-            randX = Random.Range(-8.4f, 8.4f);
+            randX = Random.Range(pos.x + offset, pos.x + (offset * 2));
             posSpawn = new Vector2(randX, transform.position.y);
             Instantiate(enemy, posSpawn, Quaternion.identity);
         }
---
+++ LELOUP/Assets/Script/EnemyIA.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIA : MonoBehaviour
+{
+    public float speed = 3;
+    private Transform targets;
+    bool facingright = true;
+    private float prevX = 0;
+    void Start()
+    {
+        targets = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targets.position, speed * Time.deltaTime);
+        
+        
+        if ( prevX > transform.position.x && !facingright)
+        {
+            Xflip();
+        }
+        else if (prevX < transform.position.x && facingright)
+        {
+            Xflip();
+        }
+
+        prevX = transform.position.x;
+    }
+
+    void Xflip()
+    {
+        facingright = !facingright;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
