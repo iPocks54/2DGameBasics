6be7f4f762e8cc38c786043215b9f35aea8354f2
iPocks54 <arthurel54@aol.com>
2020-10-23 19:14:07 +0900

    Menu Scene

---
+++ LELOUP/Assets/Script/CameraMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMenu : MonoBehaviour
+{
+    public float speed = 1;
+    void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+}
