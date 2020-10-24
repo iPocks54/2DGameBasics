5b17adba88122bbf9cf0a5dd909bb5ff8aa4d359
Lucas Hemmerlé <lucas.hemmerle@epitech.eu>
2020-10-24 19:10:12 +0900

    Added new enemy and animations and hitboxes

---
+++ LELOUP/Assets/Script/Collision/PlayerCollide.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollide : MonoBehaviour
+{
+    private int colliderCheck;
+    private BoxCollider2D box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliderCheck = 0;
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Attack") && colliderCheck == 0) {
+            Debug.Log("Salut ça hit");
+            colliderCheck += 1;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        colliderCheck = 0;
+    }
+}
---
+++ LELOUP/Assets/Script/Enemies/MedievalAI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedievalAI : MonoBehaviour {
+    public float speed = 2f;
+    public bool facingright;
+    private float prevX = 0;
+    public GameObject target;
+    private Vector3 pos;
+    private Animator animator;
+    private GameObject player;
+    private bool attacking;
+    private BoxCollider2D box;
+    private float attackTimer = 0;
+    private float attackCd = 0.8f; 
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        pos = player.transform.position;
+        animator = GetComponent<Animator>();
+        attacking = false;
+    }
+
+    void Update() {
+
+        pos = player.transform.position;
+
+        HandleMovement();
+        HandleRotation();
+        Attacks();
+
+    }
+
+    void Attacks() {
+        if (Mathf.Abs(transform.position.x - pos.x) <= 1.2 && !attacking && attackTimer <= 0) {
+            attacking = true;
+            attackTimer = attackCd;
+            animator.Play("Attack1");
+        } 
+        if (attacking) {
+            if (attackTimer > 0)
+                attackTimer -= Time.deltaTime;
+            else
+                attacking = false;
+        }
+    }
+
+    void HandleMovement() {
+        if (pos.x < transform.position.x && !attacking) {
+            transform.position += -Vector3.right * speed * Time.deltaTime;
+            animator.Play("Run");
+        } else if (pos.x > transform.position.x && !attacking) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            animator.Play("Run");
+        }
+    }
+
+    void HandleRotation() {
+        if (prevX > transform.position.x && !facingright && !attacking) {
+            Xflip();
+        } else if (prevX < transform.position.x && facingright && !attacking) {
+            Xflip();
+        }
+        prevX = transform.position.x;
+    }
+
+    void Xflip() {
+        facingright = !facingright;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
